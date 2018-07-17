08b3a2e55b93c627087ccae8b0dbbb828a4d9d8c
Gbaja <gbajaf@yahoo.co.uk>
2018-07-17 22:49:19 +0100

    router to add new group-back end api call

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const signup = require("./signup.js");
 const login = require("./login.js");
+const newGroup = require("./new_group");
 
 router.get("/api/hello", (req, res) => {
   res.send("Hello world");
 });
 router.post("/api/signup", signup.post);
 router.post("/api/login", login.post);
+router.post("/api/new_group", newGroup.post);
 
 module.exports = router;
--- server/routes/new_group.js
+++ server/routes/new_group.js
@@ -1,3 +1,27 @@
-//check if the name of group has already beeen used
-//if yes, 422 error for name exists
-//if no, add group
+const checkGroupName = require("../queries/check_group_name");
+const addGroup = require("../queries/new_group");
+
+exports.post = async (req, res) => {
+  const { name, description, created_by_id } = req.body;
+
+  try {
+    const nameExists = await checkGroupName(name);
+    if (!nameExists) {
+      const groupDetails = {
+        name,
+        description,
+        created_by_id
+      };
+      const newGroupInfo = await addGroup(groupDetails);
+      console.log(newGroupInfo);
+      res.send({ type: "success", message: "New group created!" });
+    } else {
+      res
+        .status(422)
+        .send({ type: "error", message: "group name has already been used!" });
+    }
+  } catch (err) {
+    res.status(500).send(err);
+    console.log("add new group :", err);
+  }
+};
