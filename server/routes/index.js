a032aa3e3f6faec2a35a706531c8c127e4cff9bf
Gbaja <gbajaf@yahoo.co.uk>
2018-07-28 13:56:51 +0100

    back end query and routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const signup = require("./signup.js");
 const login = require("./login.js");
 const newGroup = require("./new_group");
+const allGroups = require("./all_group");
 
 router.get("/api/hello", (req, res) => {
   res.send("Hello world");
@@ -10,5 +11,6 @@ router.get("/api/hello", (req, res) => {
 router.post("/api/signup", signup.post);
 router.post("/api/login", login.post);
 router.post("/api/new_group", newGroup.post);
+router.get("/api/all_groups", allGroups.get);
 
 module.exports = router;
